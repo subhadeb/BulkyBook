297eb821fcdb1683678b4d80af0debd378cbeecf
Subha Deb <subdeb@deloitte.com>
2020-11-13 21:35:06 +0530

    After 82 Category js and Index Datatable Working

--- BulkyBook/BulkyBook/DataSubha/Scripts.sql
+++ BulkyBook/BulkyBook/DataSubha/Scripts.sql
@@ -1,4 +1,4 @@
---For Inserting Data to Book Table.
+-->>1>>For Inserting Data to Book Table.
 
 
 ----SELECT ROUND(RAND() * (MaxRang - MinRange) + MinRange,0)
@@ -7,3 +7,15 @@ INSERT INTO Book VALUES(
 'SD' + CHAR(ROUND(RAND() * (90 - 65) + 65,0)),
 'ISBN' + CAST(ROUND(RAND() * 10000,0) AS NVARCHAR)
 )
+
+
+
+-->>2>>For Inserting Data to Category Table
+Insert into Categories values('Fiction');
+Insert into Categories values('Drama');
+
+
+
+
+
+
