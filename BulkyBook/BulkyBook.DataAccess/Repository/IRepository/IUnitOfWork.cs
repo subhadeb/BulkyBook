ee1d8b23083fb88f03af204a04a0832163d80ec9
Subha Deb <subdeb@deloitte.com>
2020-11-14 09:42:29 +0530

    After 86 Category Upsert Post Action Post is working

--- BulkyBook/BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook/BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,5 +9,6 @@ namespace BulkyBook.DataAccess.Repository.IRepository
         ICategoryRepository Category { get; }
 
         ISP_Call SP_Call { get; }
+        void Save();
     }
 }
--- BulkyBook/BulkyBook/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Areas.Admin.Controllers
@@ -21,8 +22,41 @@ namespace BulkyBook.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == null)
+            {
+                return View(category); ;
+            }
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
 
+            }
+            return View(category);
+        }
 
         #region API Calls
 
