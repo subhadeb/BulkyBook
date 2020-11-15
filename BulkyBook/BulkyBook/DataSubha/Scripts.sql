ab68bd5dd04319d5aec6ce5268e1b19793147c78
Subha Deb <subdeb@deloitte.com>
2020-11-15 22:58:45 +0530

    After 110 Home Page, Section 9 Product CRUD Complete

--- BulkyBook/BulkyBook/DataSubha/Scripts.sql
+++ BulkyBook/BulkyBook/DataSubha/Scripts.sql
@@ -15,7 +15,18 @@ Insert into Categories values('Fiction');
 Insert into Categories values('Drama');
 
 
+-->3 Insert Products Copy
+
+insert into Products
+(Title,Description ,Author,ListPrice ,Price,Price50,Price100,CategoryId,CoverTypeId,ISBN,ImageUrl)
+select top 1 
+Title,Description ,Author,ListPrice ,Price,Price50,Price100,CategoryId,CoverTypeId,ISBN,ImageUrl
+from Products
 
 
+-->>4 Updte products
 
-
+Update Products set Title = 'Product Two' where Id=2
+Update Products set Title = 'Product Three' where Id=3
+Update Products set Title = 'Product Four' where Id=4
+Update Products set Title = 'Product Five' where Id=5
