29f22c645d99340e9b029b6e55715d76d310e94a
Subha Deb <subdeb@deloitte.com>
2020-11-16 10:50:45 +0530

    After 119 Solution  Company Index and Upsert View , CRUD On Company Done

--- BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -15,6 +15,7 @@ namespace BulkyBook.DataAccess.Repository
             Category = new CategoryRepository(_db);
             CoverType = new CoverTypeRepository(_db);
             Product = new ProductRepository(_db);
+            Company = new CompanyRepository(_db);
             SP_Call = new SP_Call(_db);
         }
 
@@ -23,6 +24,8 @@ namespace BulkyBook.DataAccess.Repository
         public ICoverTypeRepository CoverType { get; private set; }
 
         public IProductRepository Product { get; private set; }
+
+        public ICompanyRepository Company { get; private set; }
         public ISP_Call SP_Call { get; private set; }
 
         public void Dispose()
