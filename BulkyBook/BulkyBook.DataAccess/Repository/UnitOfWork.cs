a59968b27b5910235adea7d2ab9f736c2042d35e
Subha Deb <subdeb@deloitte.com>
2020-11-16 21:39:40 +0530

    After 121 Solution  Add Application User to Repository and Section 10 Company and User Complete

--- BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -16,6 +16,7 @@ namespace BulkyBook.DataAccess.Repository
             CoverType = new CoverTypeRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
             SP_Call = new SP_Call(_db);
         }
 
@@ -26,6 +27,8 @@ namespace BulkyBook.DataAccess.Repository
         public IProductRepository Product { get; private set; }
 
         public ICompanyRepository Company { get; private set; }
+
+        public IApplicationUserRepository ApplicationUser { get; private set; }
         public ISP_Call SP_Call { get; private set; }
 
         public void Dispose()
