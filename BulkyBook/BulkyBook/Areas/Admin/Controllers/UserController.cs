fa16afdb96db4ca7745c19a4484ab83059aa285a
Subha Deb <subdeb@deloitte.com>
2020-11-18 10:37:14 +0530

    After 135 Final Lock Unlock API Call and Section 12 User Management Complete

--- BulkyBook/BulkyBook/Areas/Admin/Controllers/UserController.cs
+++ BulkyBook/BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -50,6 +50,27 @@ namespace BulkyBook.Areas.Admin.Controllers
             return Json(new { data = userList });
         }
 
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(x => x.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error in Locking Unlocking" });
+            }
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                //user is currently locked, we will unlock them.
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Operation Succesfull" });
+        }
+
         #endregion
     }
 }
