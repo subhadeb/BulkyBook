707b7fb1c7e3d9ed4caced50617d5527b213c4af
Subha Deb <subdeb@deloitte.com>
2020-11-27 17:49:41 +0530

    After 196 Ship Order and Cancel Order

--- BulkyBook/BulkyBook/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBook/BulkyBook/Areas/Admin/Controllers/OrderController.cs
@@ -9,6 +9,7 @@ using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace BulkyBook.Areas.Admin.Controllers
 {
@@ -41,11 +42,57 @@ namespace BulkyBook.Areas.Admin.Controllers
             return View(OrderVM);
         }
 
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult StartProcessing(int id)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(x => x.Id == id);
+            orderHeader.OrderStatus = SD.StatusInProcess;
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult ShipOrder()
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id);
+            orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
+            orderHeader.OrderStatus = SD.StatusShipped;
+            orderHeader.ShippingDate = DateTime.Now;
 
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
 
-            #region API Calls
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult CancelOrder(int id)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(x => x.Id == id);
+            if (orderHeader.PaymentStatus == SD.PaymentStatusApproved)
+            {
+                var options = new RefundCreateOptions
+                {
+                    Amount = Convert.ToInt32(orderHeader.OrderTotal * 100),
+                    Reason = RefundReasons.RequestedByCustomer,
+                    Charge = orderHeader.TransactionId
+                };
+                var service = new RefundService();
+                Refund refund = service.Create(options);
+                orderHeader.OrderStatus = SD.StatusRefunded;
+                orderHeader.PaymentStatus = SD.StatusRefunded;
+            }
+            else
+            {
+                orderHeader.OrderStatus = SD.StatusCancelled;
+                orderHeader.PaymentStatus = SD.StatusCancelled;
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
 
-            [HttpGet]
+        #region API Calls
+
+        [HttpGet]
         public IActionResult GetOrderList(string status)
         {
             var claimsIdentity = (ClaimsIdentity)User.Identity;
@@ -60,7 +107,7 @@ namespace BulkyBook.Areas.Admin.Controllers
             else
             {
                 orderHeaderList = _unitOfWork.OrderHeader.GetAll(
-                    x=>x.ApplicationUserId == claim.Value ,includeProperties: "ApplicationUser");
+                    x => x.ApplicationUserId == claim.Value, includeProperties: "ApplicationUser");
             }
             switch (status)
             {
@@ -85,7 +132,7 @@ namespace BulkyBook.Areas.Admin.Controllers
             }
 
 
-            return Json(new { data = orderHeaderList});
+            return Json(new { data = orderHeaderList });
         }
         #endregion
 
